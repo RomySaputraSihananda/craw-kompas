2313e79baf24170b592b3ee483729870807f688a
romys <romysaputrasihanandaa@gmail.com>
2023-11-30 17:11:41 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from requests import sessions as request;
+
+
+# https://indeks.kompas.com/?site=news&date=2023-11-30&page=1
+
+print(pq(request.Session().get('http://www.kompas.com/global/read/2023/08/02/095400370/wali-kota-di-meksiko-dikecam-karena-hadirkan-penari-striptis-dalam').text)('title').text());
