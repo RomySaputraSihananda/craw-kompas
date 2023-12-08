48ed6483db9bfcf06c9f49727b5659e4ed62313c
romys <romysaputrasihanandaa@gmail.com>
2023-12-08 22:06:04 +0700

    update README

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from lib import Kompas
 from lib.helpers import Site
 from json import dumps
 
+if(__name__ == '__main__'):
+    kompas: Kompas = Kompas()
 
-kompas: Kompas = Kompas()
-
-with open('data/bola.json', 'w') as file:
-    file.write(dumps(kompas.execute(site=Site.Global, page=1, date='2020-10-11')))
+    with open(f'data/{Site.Global.name}.json', 'w') as file:
+        file.write(dumps(kompas.execute(site=Site.Global, page=1, date='2020-10-11')))
